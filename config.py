cb0153e8ef1e56e138f5fc4f066468f855ae14d3
Javier Vega <javiespiri@hotmail.com>
2026-06-11 18:19:10 -0600

    github test

--- config.py
+++ config.py
@@ -4,6 +4,8 @@ from pathlib import Path
 
 load_dotenv()
 
+print("DB_HOST exists:", os.getenv("DB_HOST") is not None)
+print("DB_HOST length:", len(os.getenv("DB_HOST", "")))
 db_host = os.getenv("DB_HOST")
 db_name = os.getenv("DB_NAME")
 db_user = os.getenv("DB_USER")
@@ -17,7 +19,8 @@ neon_pw = os.getenv("NEON_PASSWORD")
 
 token_1 = os.getenv("API_TOKEN_FOOTBALL")
 sheet_id = os.getenv("SHEET_ID")
-
+print("NAME_MAP exists:", os.getenv("NAME_MAP") is not None)
+print("NAME_MAP length:", len(os.getenv("NAME_MAP", "")))
 name_map = json.loads(os.getenv("NAME_MAP"))
 
 PROJECT_ROOT = Path(__file__).resolve().parent
